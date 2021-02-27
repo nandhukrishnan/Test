ffba8b7eb3e8ef87831992ad6b27f59a53366a81
beenupb <beenupb@gmail.com>
2021-02-27 12:47:20 +0530

    Added Subtraction function

--- Calc.c
+++ Calc.c
@@ -2,9 +2,15 @@
 void main()
 {
 	sum(5, 10);
+	sub(8,4);
 }
 void sum(int a, int b)
 {
 	int c = a + b;
 	printf("SUM: %d", c);
+}
+void sub(int a, int b)
+{
+	int c = a - b;
+	printf("SUB: %d", c);
 }
